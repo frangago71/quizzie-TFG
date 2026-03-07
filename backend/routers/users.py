9254ad77dd8a564588a89ddcaa2a11d55924bb21
Fran Gago <frangago71@gmail.com>
2026-03-07 03:17:54 +0100

    feat: create routes for teacher's quizzes

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -3,6 +3,7 @@ from sqlmodel import Session, select
 from typing import List
 from ..database import get_session
 from ..models.users import Teacher, Group, Nickname, TeacherRead
+from ..models.quizzes import Quiz
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -10,6 +11,14 @@ router = APIRouter(prefix="/users", tags=["Users"])
 def read_teachers(session: Session = Depends(get_session)):
     """Devuelve los profesores con la contraseña censurada."""
     return session.exec(select(Teacher)).all()
+
+def get_current_teacher_id():
+    # Se modificará cuando se implemente la gestión de usuarios y autenticación
+    return 1
+
+@router.get("/{teacher_id}/quizzes/", response_model=List[Quiz])
+def get_teacher_quizzes(teacher_id: int = Depends(get_current_teacher_id), session: Session = Depends(get_session)):
+    return session.exec(select(Quiz).where(Quiz.teacher_id == teacher_id)).all()
 
 @router.get("/groups", response_model=List[Group])
 def read_groups(session: Session = Depends(get_session)):
