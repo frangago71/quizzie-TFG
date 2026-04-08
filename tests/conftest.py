98979657f26808c6cad294125497373682e3d729
Fran Gago <frangago71@gmail.com>
2026-04-09 00:22:51 +0200

    test: modify conftest to run integration tests
    Refs #18

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,30 @@
 import pytest
 from fastapi.testclient import TestClient
-from backend.main import app  
+from sqlmodel import Session, SQLModel, create_engine, StaticPool
+from main import app
+from database import get_session
 
-@pytest.fixture
-def client():
-    with TestClient(app) as c:
-        yield c
+
+sqlite_url = "sqlite://"
+engine = create_engine(
+    sqlite_url,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
+@pytest.fixture(name="session")
+def session_fixture():
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+    SQLModel.metadata.drop_all(engine)
+
+@pytest.fixture(name="client")
+def client_fixture(session):
+    def get_session_override():
+        return session
+    
+    app.dependency_overrides[get_session] = get_session_override
+    with TestClient(app) as client:
+        yield client
+    app.dependency_overrides.clear()
