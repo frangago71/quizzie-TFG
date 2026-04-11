95a0f6ebc99635e8e1058eba3d98cca1046b01d8
Fran Gago <frangago71@gmail.com>
2026-04-11 17:37:48 +0200

    fix: change answer and participant relationship to match reqs

--- backend/models/rooms.py
+++ backend/models/rooms.py
@@ -45,8 +45,10 @@ class Participant(SQLModel, table=True):
     
     room_id: int = Field(foreign_key="room.id")
     room: Room = Relationship(back_populates="participants")
-    
-    answers: List["Answer"] = Relationship(back_populates="participant")
+    answers: List["Answer"] = Relationship(
+        back_populates="participant",
+        sa_relationship_kwargs={"cascade": "all, delete-orphan"}
+    )
 
 class Answer(SQLModel, table=True): 
     id: Optional[int] = Field(default=None, primary_key=True)
