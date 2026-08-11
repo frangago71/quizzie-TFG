193d35beb65280eac0f3cd80e44821291138499d
Fran Gago <frangago71@gmail.com>
2026-08-11 11:59:55 +0200

    build: update vite conf with chunks to avoid size warnings running E2E tests
    Refs #92

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,15 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+          icons: ["lucide-react"],
+          qr: ["html5-qrcode", "qrcode.react"],
+        },
+      },
+    },
   },
   test: {
     globals: true,
